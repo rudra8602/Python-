b923bf409c0e80033245d8f144de74e635d5bb5b
Rudransh Khandelwal <162789390+rudra8602@users.noreply.github.com>
2025-12-20 15:22:44 +0530

    Add files via upload

---
+++ Program16.py
@@ -0,0 +1,16 @@
+birthdays = {'Alice': 'Apr 1', 'Bob': 'Dec 12', 'Carol': 'Mar 4'}
+
+while True:
+    print('Enter a name: (press q to quit)')
+    name = input()
+    if name == 'q':
+        break
+
+    if name in birthdays:
+        print(birthdays[name] + ' is the birthday of ' + name)
+    else:
+        print('I do not have birthday information for ' + name)
+        print('When is their birthday?')
+        bday = input()
+        birthdays[name] = bday
+        print('Birthday database updated.')
